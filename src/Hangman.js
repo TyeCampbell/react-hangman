329507b01bdaff1e0b83f0522f832064d3f6523e
Tye Campbell <tylerjoelcampbell@gmail.com>
2019-11-25 19:44:59 -0600

    removed jpg images and replaced with new png vector imagaes that are better

--- src/Hangman.js
+++ src/Hangman.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 import "./Hangman.css";
 import { randomWord } from "./words";
-import img0 from "./0.jpg";
-import img1 from "./1.jpg";
-import img2 from "./2.jpg";
-import img3 from "./3.jpg";
-import img4 from "./4.jpg";
-import img5 from "./5.jpg";
-import img6 from "./6.jpg";
+import img0 from "./0.png";
+import img1 from "./1.png";
+import img2 from "./2.png";
+import img3 from "./3.png";
+import img4 from "./4.png";
+import img5 from "./5.png";
+import img6 from "./6.png";
 
 class Hangman extends Component {
   /** by default, allow 6 guesses and use provided gallows images. */
